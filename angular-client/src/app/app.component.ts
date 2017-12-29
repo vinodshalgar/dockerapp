da7c070748f94ff0f1951195a626d1148ff41dd4
Vinod <vinod.shalgar@gmail.com>
2017-12-29 15:08:31 +0530

    Update app.component.ts

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   title = 'app works!';
 
   // Link to our api, pointing to localhost
-  API = 'http://localhost:3000';
+  API = 'http://52.230.83.134:3000';
 
   // Declare empty list of people
   people: any[] = [];
