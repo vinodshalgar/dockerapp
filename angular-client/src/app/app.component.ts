e9781211dd9060c8574ab9c8eb3abe21af142b1b
Vinod Narsing Shalgar <vinod.shalgar@oneadvanced.com>
2017-12-29 16:17:30 +0530

    test changes

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -12,7 +12,7 @@ import 'rxjs/add/operator/map';
 export class AppComponent implements OnInit {
   title = 'app works!';
 
-  // Link to our api, pointing to localhost test changes
+  // Link to our api, pointing to localhost 
   API = 'http://52.230.83.134:3000';
 
   // Declare empty list of people
